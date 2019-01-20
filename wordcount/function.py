93edff62083b8fd89148754ad919c938cdef8379
word_count <124738501@qq.com>
2019-01-20 17:05:16 +0800

    19年1月20日
    增加about网页说明统计网站用途，并且增加了方便去到各个页面的按钮

--- wordcount/function.py
+++ wordcount/function.py
@@ -1,4 +1,4 @@
-#编写函数传递指令给各个页面
+#编写函数
 from django.shortcuts import render
 
 
@@ -10,15 +10,19 @@ def count(request):
 	'''显示统计结果'''
 	text_req = request.GET['text']
 	total_count = len(text_req)
-	
+
 	count_dic = {}
 	for word in text_req:
 		if word not in count_dic:
 			count_dic[word] = 1
 		else:
 			count_dic[word] += 1
-	sorted_dic = sorted(count_dic.items(), key=lambda w:w[1], reverse=False)			#排序
-	
-	return render(request, 'count.html',
-				 {'count':total_count, 'text':text_req, 
-				 'countdic':count_dic, 'sorted':sorted_dic})
+	sorted_count = sorted(count_dic.items(), key=lambda w:w[1], reverse=True)
+
+	return render(request, 'count.html', 
+			{'count': total_count, 'text':text_req, 'sort':sorted_count})
+
+
+def about(request):
+	'''关于本网站'''
+	return render(request, 'about.html')
--- wordcount/urls.py
+++ wordcount/urls.py
@@ -1,4 +1,4 @@
-"""wordcount_2 URL Configuration
+"""wordcount_3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -20,5 +20,7 @@ from . import function
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', function.home),
-    path('count', function.count),
+    path('count/', function.count),
+    path('about', function.about),
+    
 ]
